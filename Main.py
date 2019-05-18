969f858080067f8bb4098628029ecd5c40a77f9c
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2019-05-17 21:06:45 -0600

    Version 2

--- Main.py
+++ Main.py
@@ -2,18 +2,8 @@
 #Juan Diego Solorzano 18151
 #Andrea Paniagua
 
-from neo4jrestclient.client import GraphDatabase
-
-db = GraphDatabase("http://localhost:7474", username="neo4j", password="mypassword")
-
-pacientes = Hospital.labels.create("Pacientes")
-
-doctores = Hospital.labels.create("Doctores")
-
-medicina = Hospital.labels.create("Medicina")
-
-
-opcion = 0
+from py2neo import *
+#db = GraphDatabase("http://localhost:7474", username="neo4j", password="mypasswordopcion = 0
 
 print("Bienvenido a Hoja de Trabajo 10 \n")
 while opcion != "6":
@@ -30,30 +20,33 @@ while opcion != "6":
         nombre = input("Ingrese el nombre del doctor")
         especialidad = input("Ingrese la especialidad del doctor")
         contacto = input("Ingrese numero de telefono del doctor")
-        
-        doc = Hospital.nodes.create(name=nombre, specialty=especialidad, contact=contacto)
-        doctores.add(doc)
+
+        db.run("CREATE (d:Doctor {name:'"+nombre+"', specialty:'"+especialidad+"', contact:'"+contacto+"'})")
+
         
     else if opcion == "2":
         #Crea un paciente con sus datos
         nombre = input("Ingrese el nombre del paciente")
         contacto = input("Ingrese numero de telefono del paciente")
 
-        pac = Hospital.nodes.create(name=nombre, contact=contacto)
-        pacientes.add(pac)
+        db.run("CREATE (p:Paciente {name:'"+nombre+"', contact:'"+contacto+"'})")
+
         
     else if opcion == "3":
         #Crea una visita con sus datos
+        nomPac = input("Ingrese el paciente al tratar")
+        nomDoc = input("Ingrese el nombre del doctor que visita")
         fecha = input("Ingrese la fecha de la visita (MM/DD/YY)")
-        doctor = input("Ingrese el nombre del doctor que visita")
-        if specialty == especialidad1:
-            receta = medicina1
-        else if specialty == especialidad2:
-            receta = medicina2
+        medicina = input("Ingrese el nombre de la medicina que le receto")
+
+        db.run("MATCH (p:Paciente),(d:Doctor),(m:Medicina) WHERE p.nombre = '"+nomPac+"' AND d.nombre = '"+nomDoc"' AND m.nombre = '"+medicina"' CREATE (p)-[r:VISITS{date: '"+ fecha +"'}]->(d) -> [r:PRESCRIBES] -> (m) -> [r: TAKES] -> (p) ")
+        
+        
         
     else if opcion == "4":
         #Busca doctores por especialidad
         esp = input("Ingrese la especialidad que busca")
+        
         
     else if opcion == "5":
         #Crea una relacion entre dos personas
@@ -65,14 +58,13 @@ while opcion != "6":
             
             d1 = input("Ingrese el nombre del primer doctor")
             d2 = input("Ingrese el nombre del segundo doctor")
-            d1.relationships.create("Conoce", d2)
-            d2.relationships.create("Conoce", d1)
+            db.run("MATCH (doc1:Doctor),(doc2:Doctor) WHERE doc1.nombre = '"+d1+"' AND doc2.nombre = '"+d2"'CREATE (doc1)<-[r:KNOWS]->(doc2)")
+            
         else if opc == "2":
             
             p1 = input("Ingrese el nombre del primer paciente")
             p2 = input("Ingrese el nombre del segundo paciente")
-            p1.relationships.create("Conoce", p2)
-            p2.relationships.create("Conoce", p1)
+            db.run("MATCH (pac1:Paciente),(pac2:Paciente) WHERE pac1.nombre = '"+p1+"' AND pac2.nombre = '"+p2"'CREATE (pac1)<-[r:KNOWS]->(pac2)")
 
         
 print("Gracias por utilizar el programa!")
