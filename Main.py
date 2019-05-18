db384d1e7375a1611b4b0735fda780b2b69d9fa5
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2019-05-17 22:23:01 -0600

    Version 3

--- Main.py
+++ Main.py
@@ -4,15 +4,15 @@
 
 from py2neo import *
 #db = GraphDatabase("http://localhost:7474", username="neo4j", password="mypasswordopcion = 0
-
+opcion = 0
 print("Bienvenido a Hoja de Trabajo 10 \n")
 while opcion != "6":
-    opcion = input("Eliga una opcion: \n
-          "1. Ingresar un Doctor \n
-          "2. Ingresar un Paciente \n
-          "3. Ingresar una visita \n
-          "4. Consultar Doctores por especialidad \n
-          "5. Crear una relacion entre dos personas \n
+    opcion = input("Eliga una opcion: \n"
+          "1. Ingresar un Doctor \n"
+          "2. Ingresar un Paciente \n"
+          "3. Ingresar una visita \n"
+          "4. Consultar Doctores por especialidad \n"
+          "5. Crear una relacion entre dos personas \n"
           "6. Salir \n")
     
     if opcion == "1":
@@ -24,7 +24,7 @@ while opcion != "6":
         db.run("CREATE (d:Doctor {name:'"+nombre+"', specialty:'"+especialidad+"', contact:'"+contacto+"'})")
 
         
-    else if opcion == "2":
+    elif opcion == "2":
         #Crea un paciente con sus datos
         nombre = input("Ingrese el nombre del paciente")
         contacto = input("Ingrese numero de telefono del paciente")
@@ -32,39 +32,44 @@ while opcion != "6":
         db.run("CREATE (p:Paciente {name:'"+nombre+"', contact:'"+contacto+"'})")
 
         
-    else if opcion == "3":
+    elif opcion == "3":
         #Crea una visita con sus datos
         nomPac = input("Ingrese el paciente al tratar")
         nomDoc = input("Ingrese el nombre del doctor que visita")
         fecha = input("Ingrese la fecha de la visita (MM/DD/YY)")
         medicina = input("Ingrese el nombre de la medicina que le receto")
 
-        db.run("MATCH (p:Paciente),(d:Doctor),(m:Medicina) WHERE p.nombre = '"+nomPac+"' AND d.nombre = '"+nomDoc"' AND m.nombre = '"+medicina"' CREATE (p)-[r:VISITS{date: '"+ fecha +"'}]->(d) -> [r:PRESCRIBES] -> (m) -> [r: TAKES] -> (p) ")
+        db.run("MATCH (p:Paciente),(d:Doctor),(m:Medicina) WHERE p.name = '"+nomPac+"' AND d.name = '"+nomDoc+"' AND m.name = '"+medicina+"' CREATE (p)-[r:VISITS{date: '"+ fecha +"'}]->(d) -> [r:PRESCRIBES] -> (m) -> [r: TAKES] -> (p) ")
         
         
         
-    else if opcion == "4":
+    elif opcion == "4":
         #Busca doctores por especialidad
         esp = input("Ingrese la especialidad que busca")
+        nomb = db.run("MACTH (d:Doctor) WHERE d.specialty = '"+esp+"' return d.name")
+        contac = db.run("MACTH (d:Doctor) WHERE d.specialty = '"+esp+"' return d.contact")
+        print("Resultados: \n"
+              "Nombre: " + nomb + "\n"
+              "Contacto: " + contac + "\n")
         
         
-    else if opcion == "5":
+    elif opcion == "5":
         #Crea una relacion entre dos personas
-        opc = input("Desea crear una relacion entre: \n
-                    "1. Doctores \n
-                    "2. Pacientes \n
+        opc = input("Desea crear una relacion entre: \n"
+                    "1. Doctores \n"
+                    "2. Pacientes \n"
                     )
         if opc == "1":
             
             d1 = input("Ingrese el nombre del primer doctor")
             d2 = input("Ingrese el nombre del segundo doctor")
-            db.run("MATCH (doc1:Doctor),(doc2:Doctor) WHERE doc1.nombre = '"+d1+"' AND doc2.nombre = '"+d2"'CREATE (doc1)<-[r:KNOWS]->(doc2)")
+            db.run("MATCH (doc1:Doctor),(doc2:Doctor) WHERE doc1.name = '"+d1+"' AND doc2.name = '"+d2+"'CREATE (doc1)<-[r:KNOWS]->(doc2)")
             
-        else if opc == "2":
+        elif opc == "2":
             
             p1 = input("Ingrese el nombre del primer paciente")
             p2 = input("Ingrese el nombre del segundo paciente")
-            db.run("MATCH (pac1:Paciente),(pac2:Paciente) WHERE pac1.nombre = '"+p1+"' AND pac2.nombre = '"+p2"'CREATE (pac1)<-[r:KNOWS]->(pac2)")
+            db.run("MATCH (pac1:Paciente),(pac2:Paciente) WHERE pac1.name = '"+p1+"' AND pac2.name = '"+p2+"'CREATE (pac1)<-[r:KNOWS]->(pac2)")
 
         
 print("Gracias por utilizar el programa!")
